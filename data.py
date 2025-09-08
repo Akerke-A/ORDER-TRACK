ec9bf5906f9b91c177a0fdf0048ba7b7d6769ea9
Akerke-A <abdullayeva.akerke@yandex.kz>
2025-09-08 11:22:49 +0100

    update data.py firstname

---
+++ data.py
@@ -0,0 +1,20 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+
+# Тело запроса на создание заказа
+user_body = {
+    "firstName": "Мария",
+    "lastname" : "Иванова",
+    "address": "г. Москва, ул. Пушкина, д. 10",
+    "metroStation": "Фрунзенская",
+    "phone": "+79995553322",
+    "rentTime": "2",
+    "deliveryDate": "2025-09-10",
+    "comment": "",
+    "color": [
+        "BLACK"
+    ]
+
+}
